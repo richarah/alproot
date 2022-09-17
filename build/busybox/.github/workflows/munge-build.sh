4d0a825c4fa2b9684b05373ac5573b70d41d724f
richarah <richarah@stud.ntnu.no>
2022-09-17 17:23:49 +0200

    Fixed build artefacts staying in folder

--- build/busybox/.github/workflows/munge-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-jq '
-	.matrix.include |= map(
-		.runs.build = "./build.sh " + (.meta.entries[].directory | @sh) + "\n" + (.runs.build | sub(" --file [^ ]+ "; " "))
-	)
-' "$@"
