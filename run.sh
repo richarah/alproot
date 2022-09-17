baf347931474fb400a783ffa3e8eebc899dab176
richarah <richarah@stud.ntnu.no>
2022-09-17 18:43:11 +0200

    Fixed gitignore (hopefully)

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd env
 export fakechroot="./usr/bin/fakechroot --use-system-libs"
 fakechroot fakeroot chroot $(dirname "$0")/env /bin/sh
--- setup.sh
+++ setup.sh
@@ -2,23 +2,22 @@
 
 BASEDIR=$PWD
 
-rm -rfv ./build/* ./env/*
-
-mkdir build
-mkdir env
+# DEBUG
+# rm -rfv ./build/*
+rm -rfv ./env/*
 
 # TODO: build from source
 cd $BASEDIR/build
 wget http://ftp.debian.org/debian/pool/main/f/fakechroot/fakechroot_2.19-3.3_all.deb
 ar vx *.deb
-tar -xf data.tar.xz -C ../env
-rm -rf $BASEDIR/build/*
+tar -xf $BASEDIR/build/data.tar.xz -C ../env
+rm -rf $BASEDIR/build/data.tar.xz
 
 # See also: making wgh shell-agnostic
-bash $BASEDIR/scripts/wgh.sh richarah/busybox
-cd $BASEDIR/build/busybox
-./build.sh && tar -xf $BASEDIR/build/busybox/stable/glibc/busybox.tar.xz -C $BASEDIR/env
+# bash $BASEDIR/scripts/wgh.sh richarah/busybox
+# cd $BASEDIR/build/busybox
+# ./build.sh
+tar -xf $BASEDIR/build/busybox/stable/glibc/busybox.tar.xz -C $BASEDIR/env
 
 # DEBUG
 # rm -rf build/*
-cd $BASEDIR/env
