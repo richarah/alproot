060db4043538cfb92d7f64554ce282b5b8533310
richarah <richarah@stud.ntnu.no>
2022-09-17 19:27:44 +0200

    It's alive!

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 BASEDIR=$PWD
 
 # DEBUG
-# rm -rfv ./build/*
+rm -rfv ./build/*
 rm -rfv ./env/*
 
 # TODO: procedural programming and building from source
@@ -11,19 +11,13 @@ cd $BASEDIR/build
 
 wget http://ftp.debian.org/debian/pool/main/b/bash/bash_5.1-2+deb11u1_amd64.deb
 ar vx $BASEDIR/build/bash_5.1-2+deb11u1_amd64.deb
-tar -xf $BASEDIR/build/data.tar.xz -C ../env
-rm -rf $BASEDIR/build/data.tar.xz
+tar -xvf $BASEDIR/build/data.tar.xz -C ../env
+rm -rfv $BASEDIR/build/data.tar.xz
 
 wget http://ftp.debian.org/debian/pool/main/f/fakechroot/fakechroot_2.19-3.3_all.deb
 ar vx $BASEDIR/build/fakechroot_2.19-3.3_all.deb
-tar -xf $BASEDIR/build/data.tar.xz -C ../env
-rm -rf $BASEDIR/build/data.tar.xz
+tar -xvf $BASEDIR/build/data.tar.xz -C ../env
+rm -rfv $BASEDIR/build/data.tar.xz
 
-# See also: making wgh shell-agnostic
-# bash $BASEDIR/scripts/wgh.sh richarah/busybox
-# cd $BASEDIR/build/busybox
-# ./build.sh
-# tar -xf $BASEDIR/build/busybox/stable/glibc/busybox.tar.xz -C $BASEDIR/env
-
-# DEBUG
-# rm -rf build/*
+rm -rfv build/*
+echo "Ready."
