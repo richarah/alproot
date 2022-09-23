6c169b2effefc63339917cd1acedd413614636d6
richarah <richarah@stud.ntnu.no>
2022-09-24 00:08:32 +0200

    alproot-build.sh modified to use pigz

--- alproot-build.sh
+++ alproot-build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build . -t alproot-env && docker export $(docker create alproot-env) -o alproot-env.tar && tar -zxvf -11 alproot-env.tar
+docker build . -t alproot-env && docker export $(docker create alproot-env) -o alproot-env.tar && pigz -11 alproot-env.tar
