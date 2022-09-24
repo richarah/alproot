5ebcf26352c881db3d2783f34440050d693f9b49
richarah <richarah@stud.ntnu.no>
2022-09-24 14:50:16 +0200

    Modified build script to output tar.xz, defacto standard in packages

--- alproot-build.sh
+++ alproot-build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build . -t alproot-env && docker export $(docker create alproot-env) -o alproot-env.tar && pigz -11 alproot-env.tar
+docker build . -t alproot-env && docker export $(docker create alproot-env) -o alproot-env.tar && tar cvf alproot-env.tar.xz --use-compress-program='xz -9eT'$(nproc) alproot-env.tar && rm -rf alproot-env.tar
