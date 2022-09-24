979ddcd4bb0522df5be47dc616f3e04a7d3475eb
Richard Alexander Haydon <72919977+richarah@users.noreply.github.com>
2022-09-25 00:22:45 +0200

    Updated alproot.sh to use host net config

--- alproot.sh
+++ alproot.sh
@@ -4,8 +4,6 @@ cd env
 DEFAULTARGS="busybox sh"
 ARGS="${@:-$DEFAULTARGS}"
 
-# Fixes for PATH and /etc/resolv.conf not updating correctly in docker
+# Fixes for PATH not updating correctly in docker
 PATH=$PATH:./bin:./usr/bin
-echo "nameserver 8.8.8.8" >> etc/resolv.conf
-
-proot -r . -0 -w / -b /dev -b /proc -b /sys -b /boot $ARGS
+proot -r . -0 -w / -b /dev -b /proc -b /sys -b /boot -b /etc/resolv.conf $ARGS
