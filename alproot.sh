46b6c7e5740d61d19dc593d654a5369cffdf1821
richarah <richarah@stud.ntnu.no>
2022-09-24 16:42:56 +0200

    Nasty hack to fix resolv.conf not updating correctly in Docker container

--- alproot.sh
+++ alproot.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cd env
+
+# Fixes for PATH and /etc/resolv.conf not updating correctly in docker
 PATH=$PATH:./bin:./usr/bin
+echo "nameserver 8.8.8.8" >> etc/resolv.conf
 proot -r . -0 -w / -b /dev -b /proc -b /sys busybox sh
