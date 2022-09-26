f083a21ef511e2dfc81253bdf60f1b472e80a4e7
richarah <richarah@stud.ntnu.no>
2022-09-26 16:16:01 +0200

    Build now outputs to img directory

--- alproot-build.sh
+++ alproot-build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build . -t alproot-env && docker export $(docker create alproot-env) -o alproot-env.tar && pv alproot-env.tar | pigz -11 > alproot-env.tar.gz && rm -rf alproot-env.tar
+docker build . -t alproot-env && docker export $(docker create alproot-env) -o alproot-env.tar && pv alproot-env.tar | pigz -11 > img/alproot-env.tar.gz && rm -rf alproot-env.tar
