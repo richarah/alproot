4771591d38ed896c3cd28b55b6f332ed087e878b
richarah <richarah@stud.ntnu.no>
2022-09-26 15:25:09 +0200

    Refactoring

--- alproot-setup.sh
+++ alproot-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-rm -rf env && mkdir env && \
-wget https://github.com/richarah/alproot/releases/download/stable/alproot-env.tar.gz && \
-tar -xzvf alproot-env.tar.gz -C env && \
+rm -rf env img && \
+mkdir env img && \
+wget -O img/alproot-env.tar.gz https://github.com/richarah/alproot/releases/download/stable/alproot-env.tar.gz && \
+tar -xzvf img/alproot-env.tar.gz -C env && \
 echo "Setup complete.\nRun ./alproot.sh to enter the Alproot environment."
