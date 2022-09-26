d24464ec3b2139ed405047360d3eca5227f35a4e
richarah <richarah@stud.ntnu.no>
2022-09-26 16:18:09 +0200

    Updated build to work with post-refactoring dir tree

--- alproot-build.sh
+++ alproot-build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+mkdir img
 docker build . -t alproot-env && docker export $(docker create alproot-env) -o alproot-env.tar && pv alproot-env.tar | pigz -11 > img/alproot-env.tar.gz && rm -rf alproot-env.tar
