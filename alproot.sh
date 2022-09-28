9aa580815ef0609b56d1b7a5e64ec30f9339a7c1
Richard Alexander Haydon <72919977+richarah@users.noreply.github.com>
2022-09-28 12:22:26 +0200

    No longer breaks on machines with odd PATH

--- alproot.sh
+++ alproot.sh
@@ -5,5 +5,5 @@ DEFAULTARGS="busybox sh"
 ARGS="${@:-$DEFAULTARGS}"
 
 # Fixes for PATH not updating correctly in docker
-PATH=$PATH:./bin:./usr/bin
+PATH=./bin:./usr/bin
 proot -r . -0 -w / -b /dev -b /proc -b /sys -b /boot -b /etc/resolv.conf $ARGS
